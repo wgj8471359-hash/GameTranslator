0440b4127cf383d549804458abb239d18231586c
wgj8471359-hash <wgj8471359@hotmail.com>
2026-09-14 21:24:29 +0800

    fix(build): add missing Rect import in OverlayManager

--- app/src/main/java/com/game/translator/OverlayManager.kt
+++ app/src/main/java/com/game/translator/OverlayManager.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.graphics.PixelFormat
+import android.graphics.Rect
 import android.graphics.drawable.GradientDrawable
 import android.hardware.display.DisplayManager
 import android.os.Build
